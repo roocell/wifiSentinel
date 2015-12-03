f02d88a2169031c21b72c9c86074d0b1c77a1cb4
Michael Russell <roocell@gmail.com>
2015-12-02 20:09:24 -0500

    freeradius module compile

--- freeradius/build_radius.sh
+++ freeradius/build_radius.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /freeradius-server
-./configure --with-openssl --with-openssl-lib-dir=/usr/local/openssl/lib --with-openssl-include-dir=/usr/local/openssl/include/
+./configure --with-openssl --with-openssl-lib-dir=/usr/local/openssl/lib --with-openssl-include-dir=/usr/local/openssl/include/  
 make
 make install
--- freeradius/fix_permissions.sh
+++ freeradius/fix_permissions.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e # fail on any error
 
+groupadd freerad
+useradd -gfreerad freerad
+
 echo '* Working around permission errors locally by making sure that "freerad" uses the same uid and gid as the host volume'
-TARGET_UID=$(stat -c "%u" /etc/freeradius)
+TARGET_UID=$(stat -c "%u" /usr/local/etc/raddb/)
 echo '-- Setting freerad user to use uid '$TARGET_UID
 usermod -o -u $TARGET_UID freerad || true
-TARGET_GID=$(stat -c "%g" /etc/freeradius)
+TARGET_GID=$(stat -c "%g" /usr/local/etc/raddb/)
 echo '-- Setting freerad group to use gid '$TARGET_GID
 groupmod -o -g $TARGET_GID freerad || true
 
-chmod 700 /etc/freeradius
-chmod 700 /etc/freeradius/mods-config/files/authorize
+chmod 700 /usr/local/etc/raddb 
+chmod 700 /usr/local/etc/raddb/mods-config/files/authorize
 chmod 777 /var/log/freeradius
 chmod 666 /var/log/freeradius/radius.log
 
-/usr/sbin/freeradius -f -xx
